208dcb2d65614682838337f5df8353612cfa99cf
helene <helene.maja@hotmail.dk>
2024-06-10 13:13:04 +0200

    add github

--- main.py
+++ main.py
@@ -20,19 +20,27 @@ print(matrix)
 
 def search_number(matrix):
     number = 1
-    
-    if number in matrix:
+    not_found = True
+    while(not_found):
+        if number not in matrix:
+            not_found = False
+            return number
+        else:
+            number += 1
+
+
 
 
 # selects all elements in column 0 from row 1
-for element in matrix[1:, 0]:
-    if element == matrix[0,0]:
-        # print(element)
+for element_col in matrix[1:, 0]:
+    if element_col == matrix[0,0]:
+        print(f"element: {element_col}")
         not_in = search_number(matrix[1:, 0])
-        print(not_in)
+        print(f"num not in column: {not_in}")
 
-# for element in matrix[0, 1:]:
-#     if element == matrix[0,0]:
-#         print(element)
-#         not_in = search_number(matrix[1:, 0])
-#         print(not_in)
+
+for element in matrix[0, 1:]:
+    if element == matrix[0,0]:
+        print(f"element: {element}")
+        not_in = search_number(matrix[0, 1:])
+        print(f"num not in row: {not_in}")
